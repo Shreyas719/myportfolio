66665481a99610911edcee6c48b14e259c144857
Shreyas719 <shreyasbhagwat4055@gmail.com>
2024-08-05 20:02:48 +0800

    contact form

--- vendor/js/script.js
+++ vendor/js/script.js
@@ -17,3 +17,20 @@ $(document).ready(function(){
         loop:true
       });
 });
+// Owl-Carousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
